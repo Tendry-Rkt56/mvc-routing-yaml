59bb512fab953e44df90815f163b5b498b80c077
Tendry <tendryzephyrin@gmail.com>
2024-11-18 19:06:21 +0100

    Création de la page d'erreur

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     
+     <style>
+          .containers {
+               position:absolute;
+               top:50%;
+               left:50%;
+               transform: translate(-50%, -50%);
+               display: flex;
+               align-items:center;
+               justify-content: center;
+               flex-direction: column;
+          }
+     </style>
+
+     <div class="containers">
+          <h3>401 PAGE NOT FOUND</h3>
+     </div>
+
+</body>
+</html>
