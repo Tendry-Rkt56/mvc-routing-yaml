8d12153d811c6a567fedaf8280e09e0e5ecefcae
Tendry <tendryzephyrin@gmail.com>
2024-11-18 19:33:26 +0100

    Création de la classe DataBase

---
+++ services/DataBase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Services;
+
+class DataBase
+{
+
+     private $conn;
+
+     public function __construct()
+     {
+          $this->conn = new \PDO('mysql:host=')     
+     }
+
+}
