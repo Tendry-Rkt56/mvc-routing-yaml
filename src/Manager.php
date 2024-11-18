5aa97ba1709f231790eb3e200b34621b12e222d5
Tendry <tendryzephyrin@gmail.com>
2024-11-18 19:42:35 +0100

    Modification du conteneur d'injection Manager.php

--- src/Manager.php
+++ src/Manager.php
@@ -2,15 +2,29 @@
 
 namespace App;
 
+use Services\DataBase;
+
 class Manager
 {
 
      private static $_instance;
+     private static $_db;
 
      public static function get()
      {
           if (self::$_instance == null) self::$_instance = new self();
           return self::$_instance;
+     }
+
+     public function getDb()
+     {
+          if (self::$_db == null) self::$_db = new DataBase();
+          return self::$_db;
+     }
+
+     public function getEntity(string $class)
+     {
+          return new $class($this->getDb());
      }
 
 
