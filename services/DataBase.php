be16759ac4958e7d05ce9748cc5b277f8aa9d5e8
Tendry <tendryzephyrin@gmail.com>
2024-11-18 19:40:11 +0100

    Finition du DataBase.php

--- services/DataBase.php
+++ services/DataBase.php
@@ -9,7 +9,15 @@ class DataBase
 
      public function __construct()
      {
-          $this->conn = new \PDO('mysql:host=')     
+          LoaderFile::set("constante", '../config/Constante.yaml');
+          $constante = LoaderFile::get("constante")['database'];
+          extract($constante);
+          $this->conn = new \PDO('mysql:host='.$host.";dbname=".$dbName.";charset=$charset", $user, $password);     
+     }
+
+     public function getDb(): \PDO
+     {
+          return $this->conn;
      }
 
 }
